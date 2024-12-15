acb37f574fc802597682a9a32553c84b4fd0e20d
Markusha <LegoAnimation2022@gmail.com>
2024-12-15 08:50:32 +0000

    Relase 1

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const { Telegraf } = require('telegraf');
 
+const Token = '7592307195:AAHhXQv2P9BhJgsWz8TKzBh2Lx2zs-ZtHb0';
+
+const bot = new Telegraf(Token);
+
+bot.start((ctx) => {
+  ctx.reply('Привет!\nЯ ФИЗРУЧКА.\nЯ помогу дотрахать эту группу');
+});
+
+bot.launch()
+  .then(() => {
+    console.log('Бот запущен!');
+  })
+  .catch((err) => {
+    console.error('Ошибка при запуске бота:', err);
+  });
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
